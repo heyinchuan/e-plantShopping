3f954254b160fe927bb6b6faa04e890d66f6e88b
heyinchuan <heyinchuan@outlook.com>
2024-12-07 17:31:10 +0800

    achive showing the product qty on cart logo

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,6 +9,15 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
+    const cart = useSelector((state) => state.cart.items); // 获取购物车的 items
+    const cartTotalItems = cart.reduce((total, item) => total + item.quantity, 0); // 计算商品总数
+    // 用于调试cartTotalItems
+    // 使用 useEffect 在 cartTotalItems 更新时触发 alert
+    // useEffect(() => {
+    //     if (cartTotalItems > 0) {
+    //         alert(`购物车商品数量：${cartTotalItems}`);
+    //     }
+    // }, [cartTotalItems]);
 
     const plantsArray = [
         {
@@ -277,7 +286,24 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        
+                            <h1 className='cart'>
+                            
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="156" height="156" fill="none"></rect>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>                               
+                                    <text x="110" y="158" fontSize="96px" fill='white'>
+                                        {cartTotalItems}
+                                    </text>
+                                </svg>
+                            </h1>
+                            
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
